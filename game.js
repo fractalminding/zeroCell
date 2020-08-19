4c6fb7383e651ecdeb4f397199e69f67f0409f19
Viktor Kozlov <osago.sarapul@gmail.com>
2020-08-19 15:21:36 +0400

    цифры выводятся на поле, подсвечиваются в зависимости от цифры

--- game.js
+++ game.js
@@ -2,11 +2,11 @@
 let table = {
     createObject: function(width, height) {
         for (let i = 0; i < height; i++) {
-            table.object[i] = table.row.create(width);
+            table.array[i] = table.row.create(width);
         }
-        console.log(table.object);
+        //console.log(table.array);
     },
-    object: {},
+    array: [],
     row: {
         create: function(length) {
             let rowArray = [];
@@ -22,8 +22,35 @@ let table = {
         }
     },
     draw: function() {
-        
+        table.array.forEach(function(row, y) {
+            row.forEach(function(cellText, x) {
+                canvas.drawRect(x * 40, y * 40, 40, 40, cellText);
+                canvas.drawTextLabel(x * 40 + 10, y * 40 + 30, '#282e28', cellText);
+            });
+        });
     }
 };
-table.createObject(10, 10);
-table.draw();
+let canvas = {
+    rectColors:
+        ['#cbeacb', '#b6d2b6', '#a2bba2', '#8ea38e', '#798c79', '#657565'],
+    drawRect(x, y, width, height, cellText) {
+        let context = canvas.context;
+        context.fillStyle = canvas.rectColors[cellText];
+        context.fillRect(x, y, width, height);
+    },
+    drawTextLabel(x, y, color, text) {
+        let context = canvas.context;
+        context.fillStyle = color;
+        context.font = "30px Arial";
+        context.fillText(text, x, y);
+    }
+}
+let game = {
+    init: function() {
+        canvas.element = document.getElementById('game');
+        canvas.context = canvas.element.getContext('2d');
+        table.createObject(10, 10);
+        table.draw();
+    }
+}
+game.init();
